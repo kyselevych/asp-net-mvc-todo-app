c5f8d02ab5d1eeccc653b4da4044fc4316676bd0
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-04-21 18:25:11 +0300

    Initiliaztion project

---
+++ TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _dbConnection;
+        public HomeController(IConfiguration configuration)
+        {
+            _dbConnection = configuration.GetConnectionString("AppDB");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TodoApp/Models/TaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp.Models
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? Deadline { get; set; }
+        public DateTime? DateExecution { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
