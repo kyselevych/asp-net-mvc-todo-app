e944ac8e6d4d4f5eff2f88124f812651a690055b
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-05-03 14:42:51 +0300

    Added: confirm on click delete button

---
+++ TodoApp/wwwroot/js/main.js
@@ -0,0 +1,15 @@
+﻿function handleClickErrorBtn(e) {
+    const target = e.target;
+
+    if (!target.classList.contains('control--delete')) return;
+
+    const message = "Are you sure you want to remove the task?";
+    const isConfirm = confirm(message);
+
+    if (!isConfirm) {
+        e.preventDefault();
+        return;
+    }
+}
+
+document.addEventListener('click', handleClickErrorBtn);
