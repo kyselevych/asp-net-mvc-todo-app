211023cdba9b197cb7399db2352d0adef275ceea
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-05-03 14:23:00 +0300

    Created: controllers - TasksController, CategoriesController

---
+++ TodoApp/Controllers/CategoriesController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using TodoApp.ViewModels;
+using Business.Repositories;
+using Business.Entities;
+
+namespace TodoApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            var categoriesList = _categoryRepository.GetList();
+            var categoriesListViewModel = _mapper.Map<List<CategoryListItemViewModel>>(categoriesList);
+
+            var categoriesIndexViewModel = new CategoriesIndexViewModel()
+            {
+                CategoriesList = categoriesListViewModel
+            };
+
+            return View(categoriesIndexViewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _categoryRepository.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var categoryModel = _categoryRepository.GetById(id);
+                var editCategoryViewModel = _mapper.Map<EditCategoryViewModel>(categoryModel);
+
+                return View(editCategoryViewModel);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditCategoryViewModel editCategoryViewModel)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                var categoryModel = _mapper.Map<CategoryModel>(editCategoryViewModel);
+                _categoryRepository.Update(categoryModel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        public ActionResult Create(CreateCategoryViewModel createCategoryViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState); // bad idea
+
+            try
+            {
+                var categoryModel = _mapper.Map<CategoryModel>(createCategoryViewModel);
+                _categoryRepository.Create(categoryModel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ TodoApp/Controllers/TasksController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Business.Repositories;
+using TodoApp.ViewModels;
+using AutoMapper;
+using Business.Entities;
+
+namespace TodoApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ITaskRepository _taskRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public TasksController(ITaskRepository taskRepository, ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _taskRepository = taskRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult Index(int? category)
+        {
+            var currentListTasks = _taskRepository.GetList("current");
+            var completedListTasks = _taskRepository.GetList("completed");
+            var listCategories = _categoryRepository.GetList();
+
+            // Filter by category
+            if (category != null && category > 0)
+            {
+                currentListTasks = currentListTasks.Where(task => task.CategoryId == category);
+                completedListTasks = completedListTasks.Where(task => task.CategoryId == category);
+            }
+
+            var currentTasksListViewModel = _mapper.Map<List<CurrentTaskItemViewModel>>(currentListTasks);
+            var completedTasksListViewModel = _mapper.Map<List<CompletedTaskItemViewModel>>(completedListTasks);
+            var categoriesListViewModel = _mapper.Map<List<CategoryListItemViewModel>>(listCategories);
+            var filterCategoriesListViewModel = _mapper.Map<List<FilterCategoryListItemViewModel>>(listCategories);
+
+            categoriesListViewModel.Insert(0, new CategoryListItemViewModel() { Id = null, Name = "None" });
+            filterCategoriesListViewModel.Insert(0, new FilterCategoryListItemViewModel() { Id = 0, Name = "All" });
+
+            var taskIndexViewModel = new TaskIndexViewModel()
+            {
+                CompletedTasksList = completedTasksListViewModel,
+                CurrentTasksList = currentTasksListViewModel,
+                FilterCategoriesList = filterCategoriesListViewModel,
+                CreateTaskForm = new CreateTaskFormViewModel()
+                {
+                    CategoriesList = categoriesListViewModel
+                }
+            };
+
+            return View(nameof(Index), taskIndexViewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _taskRepository.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        public ActionResult Perform(int id)
+        {
+            try
+            {
+                _taskRepository.Perform(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        public ActionResult Create(CreateTaskViewModel createTaskViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState); // bad idea
+
+            try
+            {
+                var taskModel = _mapper.Map<TaskModel>(createTaskViewModel);
+                _taskRepository.Create(taskModel);
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
