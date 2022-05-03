2f051de6e478b99e07ac8d871e4fbe0405aa09b1
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-05-03 14:05:02 +0300

    Created: MicrosoftSQLServerDb project with repositories

---
+++ MicrosoftSQLServerDB/Repositories/CategoryRepository.cs
@@ -0,0 +1,74 @@
+﻿using Business.Entities;
+using Business.Repositories;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace MicrosoftSQLServerDb.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private string _stringConnection { get; set; }
+
+        public CategoryRepository(IConfiguration configuration)
+        {
+            _stringConnection = configuration.GetConnectionString("AppDB");
+        }
+
+        public IEnumerable<CategoryModel> GetList()
+        {
+            string query = @"SELECT Id, Name FROM Categories";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                return connection.Query<CategoryModel>(query, connection);
+            }
+        }
+
+        public CategoryModel GetById(int id)
+        {
+            string query = @"SELECT Id, Name FROM Categories WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                return connection.QuerySingle<CategoryModel>(query, new { Id = id });
+            }
+        }
+
+        public void Delete(int id)  
+        {
+            string query = @"DELETE FROM Categories WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, new { Id = id });
+
+                if (affectedRows == 0) throw new InvalidOperationException();
+            }
+        }
+
+        public void Create(CategoryModel category)
+        {
+            string query = @"INSERT INTO Categories (Name) VALUES (@Name)";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, category);
+
+                if (affectedRows == 0) throw new InvalidOperationException();
+            }   
+        }
+
+        public void Update(CategoryModel category)
+        {
+            string query = @"UPDATE Categories SET Name = @Name WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, category);
+
+                //if (affectedRows == 0) throw new InvalidOperationException();
+            }  
+        }
+    }
+}
---
+++ MicrosoftSQLServerDB/Repositories/TaskRepository.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+using Business.Repositories;
+using Business.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace MicrosoftSQLServerDb.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private string _stringConnection { get; set; }
+
+        public TaskRepository(IConfiguration configuration)
+        {
+            _stringConnection = configuration.GetConnectionString("AppDB");
+        }
+
+        public IEnumerable<TaskModel> GetList(string? status)
+        {
+            string conditionIsDone = "WHERE";
+            string orderBy = "ORDER BY";
+
+            switch (status?.ToLower())
+            {
+                case "completed":
+                    {
+                        conditionIsDone += " IsDone = 1";
+                        orderBy += " DateExecution DESC";
+                        break;
+                    }
+                case "current":
+                    {
+                        conditionIsDone = "WHERE IsDone = 0";
+                        orderBy += " CASE WHEN Deadline IS NULL THEN 1 ELSE 0 END";
+                        break;
+                    }
+                default:
+                    {
+                        conditionIsDone = "";
+                        orderBy = "";
+                        break;
+                    }
+            }
+
+            string query = $@"
+                SELECT t.Id, t.Name, t.IsDone, t.Deadline, t.DateExecution, t.CategoryId, c.Id, c.Name
+                FROM Tasks t 
+                LEFT OUTER JOIN Categories c ON (t.CategoryId = c.Id)
+                {conditionIsDone}
+                {orderBy}
+            ";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                return connection.Query<TaskModel, CategoryModel, TaskModel>(query, map: (taskModel, categoryModel) => {
+                    taskModel.Category = categoryModel;
+
+                    return taskModel;
+                });
+            }
+        }
+
+        public TaskModel GetById(int id)
+        {
+            string query = @"
+                SELECT t.Id, t.Name, t.IsDone, t.Deadline, t.DateExecution, t.CategoryId
+                FROM Tasks t
+                WHERE Id = @Id
+            ";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                return connection.QuerySingle<TaskModel>(query, new { Id = id });
+            }
+        }
+
+        public void Delete(int id)
+        {
+            string query = @"DELETE FROM Tasks WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, new { Id = id });
+
+                if (affectedRows == 0) throw new InvalidOperationException();
+            }
+        }
+
+        public void Create(TaskModel task)
+        {
+            string query = @"
+                INSERT INTO Tasks (Name, Deadline, CategoryId)
+                VALUES (@Name, @Deadline, @CategoryId)
+            ";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, task);
+
+                if (affectedRows == 0) throw new InvalidOperationException();
+            }
+        }
+
+        public void Perform(int id)
+        {
+            string query = @"
+                UPDATE Tasks
+                SET IsDone = 1, DateExecution = @DateTimeNow
+                WHERE Id = @Id
+            ";
+
+            using (var connection = new SqlConnection(_stringConnection))
+            {
+                int affectedRows = connection.Execute(query, new { Id = id, DateTimeNow = DateTime.Now });
+
+                if (affectedRows == 0) throw new InvalidOperationException();
+            }
+        }
+    }
+}
